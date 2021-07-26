e3780ea75b721be7ac39492ecaeeafc315b85f7c
mar783 <marc.zjc@gmail.com>
2021-07-26 19:29:25 +0200

    Add files via upload

---
+++ src/pl/TeleBookApp.java
@@ -0,0 +1,8 @@
+package pl;
+
+public class TeleBookApp {
+    public static void main(String[] args) {
+        TeleBookController controller = new TeleBookController();
+        controller.loop();
+    }
+}
